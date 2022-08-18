af92d492205acba02a26862e22821988377683c0
Ke Yang <yangke687@gmail.com>
2022-08-18 20:12:57 +0800

    feat: grid、flex layout

--- src/auth-app.tsx
+++ src/auth-app.tsx
@@ -1,14 +1,54 @@
 import { useAuth } from "context/auth-context";
 import React from "react";
+import styled from "@emotion/styled";
 import { ProjectListScreen } from "screens/project-list";
 
 export const AuthApp = () => {
   const { logout } = useAuth();
 
   return (
-    <div>
-      <ProjectListScreen />
-      <button onClick={() => logout()}>登出</button>
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={() => logout()}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  grid-template-areas:
+    "header"
+    "main";
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`;
