2d7352f7e939913481cd3112a277d94e0077f4f5
Ke Yang <yangke687@gmail.com>
2022-09-08 12:04:55 +0800

    feat: create error-boundary class component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import { AuthApp } from "auth-app";
 import { useAuth } from "context/auth-context";
 import { UnAuthApp } from "unauth-app";
 import "./App.css";
+import { ErrorBoundary } from "./components/error-boundary";
+import { FullPageFallback } from "./components/lib";
+
 // import { ProjectListScreen as ProjectList } from "./screens/project-list";
 
 function App() {
   const { user } = useAuth();
-  return <div className="App">{user ? <AuthApp /> : <UnAuthApp />}</div>;
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageFallback}>
+        {user ? <AuthApp /> : <UnAuthApp />}
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
