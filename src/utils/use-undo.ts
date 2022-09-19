6706ec6c2dc093294f33ea713d0af2b605df7771
Ke Yang <yangke687@gmail.com>
2022-09-19 20:38:41 +0800

    feat: create use-undo custom hook

---
+++ src/utils/use-undo.ts
@@ -0,0 +1,81 @@
+import { useCallback, useState } from "react";
+
+export const useUndo = <T>(initValue: T) => {
+  const [state, setState] = useState<{
+    history: T[];
+    current: T;
+    future: T[];
+  }>({
+    history: [],
+    current: initValue,
+    future: [],
+  });
+
+  const undo = useCallback(() => {
+    setState((currentState) => {
+      const { history, current, future } = currentState;
+
+      if (history.length === 0) {
+        return currentState;
+      }
+
+      const prev = history[history.length - 1];
+      const newFuture = [current, ...future];
+      const newHistory = history.slice(0, history.length - 1);
+
+      return {
+        history: newHistory,
+        current: prev,
+        future: newFuture,
+      };
+    });
+  }, []);
+
+  const redo = useCallback(() => {
+    setState((currentState) => {
+      const { history, current, future } = currentState;
+
+      if (future.length === 0) {
+        return currentState;
+      }
+
+      const next = future[0];
+      const newHistory = [...history, current];
+      const newFuture = future.slice(1);
+
+      return {
+        history: newHistory,
+        current: next,
+        future: newFuture,
+      };
+    });
+  }, []);
+
+  const set = useCallback((newCurrent: T) => {
+    setState((currentState) => {
+      const { history, current } = currentState;
+
+      if (newCurrent === current) {
+        return currentState;
+      }
+
+      const newHistory = [...history, current];
+
+      return {
+        history: newHistory,
+        current: newCurrent,
+        future: [],
+      };
+    });
+  }, []);
+
+  const reset = useCallback((newCurrent: T) => {
+    setState({
+      history: [],
+      current: newCurrent,
+      future: [],
+    });
+  }, []);
+
+  return [state, { undo, redo, set, reset }];
+};
