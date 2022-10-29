a75e8e5a9a07a118cebf2e5219c474a99debe127
Ke Yang <yangke687@gmail.com>
2022-10-29 21:48:16 +0800

    refactor: use react-query to add/update project

--- src/utils/use-projects.ts
+++ src/utils/use-projects.ts
@@ -1,7 +1,7 @@
 import { useHttp } from "./http";
 import { Project } from "../screens/project-list/list";
 import { useAsync } from "./use-async";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 export const useProjects = (params?: Partial<Project>) => {
   const client = useHttp();
@@ -13,28 +13,32 @@ export const useProjects = (params?: Partial<Project>) => {
 
 export const useEditProject = () => {
   const client = useHttp();
+  const queryClient = useQueryClient();
 
-  const { run, ...result } = useAsync<Project[]>();
-
-  const mutate = (params: Partial<Project>) =>
-    run(client(`projects/${params.id}`, { data: params, method: "PATCH" }));
-
-  return {
-    mutate,
-    ...result,
-  };
+  return useMutation(
+    (params: Partial<Project>) =>
+      client(`projects/${params.id}`, {
+        method: "PATCH",
+        data: params,
+      }),
+    {
+      onSuccess: () => queryClient.invalidateQueries("projects"),
+    }
+  );
 };
 
 export const useAddProject = () => {
   const client = useHttp();
+  const queryClient = useQueryClient();
 
-  const { run, ...result } = useAsync<Project[]>();
-
-  const mutate = (params: Partial<Project>) =>
-    run(client(`projects/${params.id}`, { data: params, method: "POST" }));
-
-  return {
-    mutate,
-    ...result,
-  };
+  return useMutation(
+    (params: Partial<Project>) =>
+      client(`projects/${params.id}`, {
+        method: "POST",
+        data: params,
+      }),
+    {
+      onSuccess: () => queryClient.invalidateQueries("projects"),
+    }
+  );
 };
