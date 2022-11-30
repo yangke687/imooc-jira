cb64f1f39b6880014930ef64d3262ca321fc7547
Ke Yang <yangke687@gmail.com>
2022-11-30 15:34:18 +0800

    feat: add board、tasks loading spinner

--- src/screens/board/index.tsx
+++ src/screens/board/index.tsx
@@ -1,27 +1,43 @@
 import React from "react";
 import { useDocumentTitle } from "../../utils";
 import { useBoards } from "../../utils/board";
-import { useBoardSearchParams, useProjectInUrl } from "./util";
+import {
+  useBoardSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "./util";
 import { BoardColumn } from "./board-column";
 import styled from "@emotion/styled";
 import { SearchPanel } from "./search-panel";
 import { ScreenContainer } from "../../components/lib";
+import { useTasks } from "../../utils/task";
+import { Spin } from "antd";
 
 export const BoardScreen = () => {
   useDocumentTitle("看板列表");
 
   const { data: project } = useProjectInUrl();
-  const { data: boards = [] } = useBoards(useBoardSearchParams());
+  const { data: boards = [], isLoading: boardIsLoading } = useBoards(
+    useBoardSearchParams()
+  );
+
+  const { isLoading: tasksIsLoading } = useTasks(useTasksSearchParams()[0]);
+
+  const isLoading = tasksIsLoading || boardIsLoading;
 
   return (
     <ScreenContainer>
       <h1>{project?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {boards.map((item) => (
-          <BoardColumn board={item} key={item.id} />
-        ))}
-      </ColumnsContainer>
+      {isLoading ? (
+        <Spin size={"large"} />
+      ) : (
+        <ColumnsContainer>
+          {boards.map((item) => (
+            <BoardColumn board={item} key={item.id} />
+          ))}
+        </ColumnsContainer>
+      )}
     </ScreenContainer>
   );
 };
