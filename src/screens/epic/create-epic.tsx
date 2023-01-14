60030e8f79377c1eaad95f09dd225e5f38331255
Ke Yang <yangke687@gmail.com>
2023-01-14 15:21:46 +0800

    feat: create / delete epic

---
+++ src/screens/epic/create-epic.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import { Button, Drawer, Form, Input, Spin } from "antd";
+import { DrawerProps } from "antd/es";
+import styled from "@emotion/styled";
+import { useAddEpic } from "../../utils/epic";
+import { useEpicsQueryKey } from "./util";
+import { ErrorBox } from "../../components/lib";
+import { useForm } from "antd/es/form/Form";
+import { useProjectIdInUrl } from "../board/util";
+
+export const CreateEpic = (
+  props: Pick<DrawerProps, "visible"> & { onClose: () => void }
+) => {
+  const { mutate: addEpic, isLoading, error } = useAddEpic(useEpicsQueryKey());
+
+  const [form] = useForm();
+
+  const projectId = useProjectIdInUrl();
+
+  const onFinish = async (values: any) => {
+    await addEpic({ ...values, projectId });
+    props.onClose();
+  };
+
+  useEffect(() => {
+    form.resetFields();
+  }, [form, props.visible]);
+
+  return (
+    <Drawer
+      visible={props.visible}
+      onClose={props.onClose}
+      forceRender={true}
+      destroyOnClose={true}
+      width={"100%"}
+    >
+      <Container>
+        {isLoading ? (
+          <Spin size={"large"} />
+        ) : (
+          <>
+            <h1>创建任务组</h1>
+            <ErrorBox error={error} />
+            <Form
+              form={form}
+              layout={"vertical"}
+              onFinish={onFinish}
+              style={{ width: "40rem" }}
+            >
+              <Form.Item
+                label={"名称"}
+                name={"name"}
+                rules={[{ required: true, message: "请输入任务组名称" }]}
+              >
+                <Input placeholder={"请输入任务组名称"} />
+              </Form.Item>
+
+              <Form.Item style={{ textAlign: "center" }}>
+                <Button
+                  loading={isLoading}
+                  type={"primary"}
+                  htmlType={"submit"}
+                >
+                  提交
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        )}
+      </Container>
+    </Drawer>
+  );
+};
+
+const Container = styled.div`
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
--- src/screens/epic/index.tsx
+++ src/screens/epic/index.tsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, ScreenContainer } from "../../components/lib";
 import { useProjectInUrl } from "../board/util";
-import { useEpics } from "../../utils/epic";
-import { useEpicSearchParams } from "./util";
-import { Button, List } from "antd";
+import { useDeleteEpic, useEpics } from "../../utils/epic";
+import { useEpicSearchParams, useEpicsQueryKey } from "./util";
+import { Button, List, Modal } from "antd";
 import dayjs from "dayjs";
 import { useTasks } from "../../utils/task";
 import { Link } from "react-router-dom";
+import { CreateEpic } from "./create-epic";
+import { Epic } from "../../types/epic";
 
 export const EpicScreen = () => {
   const { data: currentProject } = useProjectInUrl();
 
   const { data: epics } = useEpics(useEpicSearchParams());
 
+  const { mutate: deleteEpic } = useDeleteEpic(useEpicsQueryKey());
+
   const { data: tasks } = useTasks({ projectId: currentProject?.id });
+
+  const [epicCreateOpen, setEpicCreateOpen] = useState(false);
+
+  const confirmDeleteEpic = (epic: Epic) => {
+    Modal.confirm({
+      title: `确定删除项目组：${epic.name}`,
+      content: "点击确定删除",
+      okText: "确定",
+      onOk() {
+        deleteEpic({ id: epic.id });
+      },
+    });
+  };
 
   return (
     <ScreenContainer>
-      <h1>{currentProject?.name}任务组</h1>
+      <Row between={true}>
+        <h1>{currentProject?.name}任务组</h1>
+        <Button onClick={() => setEpicCreateOpen(true)} type={"link"}>
+          创建任务组
+        </Button>
+      </Row>
+
       <List
+        style={{ overflow: "scroll" }}
         dataSource={epics || []}
         itemLayout={"vertical"}
         renderItem={(epic) => (
@@ -27,7 +51,9 @@ export const EpicScreen = () => {
               title={
                 <Row between={true}>
                   <span>{epic.name}</span>
-                  <Button type={"link"}>删除</Button>
+                  <Button type={"link"} onClick={() => confirmDeleteEpic(epic)}>
+                    删除
+                  </Button>
                 </Row>
               }
               description={
@@ -43,6 +69,7 @@ export const EpicScreen = () => {
                 .map((task, idx) => {
                   return (
                     <Link
+                      style={{ marginRight: 5 }}
                       key={idx}
                       to={`/projects/${currentProject?.id}/board?editingTaskId=${task.id}`}
                     >
@@ -53,6 +80,10 @@ export const EpicScreen = () => {
             </>
           </List.Item>
         )}
+      />
+      <CreateEpic
+        visible={epicCreateOpen}
+        onClose={() => setEpicCreateOpen(false)}
       />
     </ScreenContainer>
   );
